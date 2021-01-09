93bd6e78b3715c8d6b71568998f1cdbb79d07cad
JerryChua <chuaxw.xwc@gmail.com>
2021-01-10 01:24:35 +0800

    background changes commit

--- script.js
+++ script.js
@@ -33,24 +33,46 @@ date();
 
 $(document).ready(function () {
     $.ajax({
-      type: "GET",
-      dataType: 'json',
-      contetType: "text/plain",
-      url: "https://api.data.gov.sg/v1/environment/psi",
-      headers: {},
-      data: {
-        "date": date()
-      },
-      success: function (data) {
-        console.log("API status: " + data.api_info.status)
-        var reading_twenty_four =
-          data.items[0].readings.psi_twenty_four_hourly;
-        var content = "";
-        $.each(reading_twenty_four, function (key, obj) {
-          console.log(key + ":" + obj);
-          content += key + ": " + obj + "<br/>";
-        });
-        $("#psi-twenty-four").html(content);
+        type: "GET",
+        dataType: 'json',
+        contetType: "text/plain",
+        url: "https://api.data.gov.sg/v1/environment/psi",
+        headers: {},
+        data: {
+            "date": date()
+        },
+        success: function (data) {
+            console.log("API status: " + data.api_info.status)
+            var reading_twenty_four =
+            data.items[0].readings.psi_twenty_four_hourly;
+            var content = "";
+            $.each(reading_twenty_four, function (key, obj) {
+                console.log(key + ":" + obj);
+                content += key + ": " + obj + "<br/>";
+            });
+            $("#psi-twenty-four").html(content);
         }
     });
 });
+
+
+
+function tod() {                                                             //time of day for background change
+    var time = new Date().getHours();
+    if (time < 6) {
+        window.location.replace("night.html");
+    }
+    else if (time < 10) {
+        window.location.replace("index.html");
+    }
+    else if (time < 17) {
+        window.location.replace("noon.html");
+    }
+    else if (time < 22) {
+        window.location.replace("dusk.html");
+    }
+    else if (time > 22) {
+        window.location.replace("night.html");
+    }
+  }
+tod();
