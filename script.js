d9d268f20cb95f728b73d4e69dca8d44ea075275
JerryChua <chuaxw.xwc@gmail.com>
2021-01-10 19:54:45 +0800

    base code done

--- script.js
+++ script.js
@@ -29,6 +29,40 @@ let date = () => {
 };
 date();
 
+let datetime = () => {
+    let d = new Date();
+    time = d.toISOString().split('.')[0]+"Z"
+}
+datetime()
+
+$(document).ready(function () {                                                     //getting the weather
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        contetType: "text/plain",
+        url: "https://api.data.gov.sg/v1/environment/air-temperature",
+        headers: {},
+        data: {
+            "date_time": time,
+            "date": date()
+        },
+        success: function (data) {
+            console.log("Weather API status: " + data.api_info.status)
+            var reading = data.items[0].readings
+            var i = 0;
+            var sum = 0;
+            $.each(reading, function (items, reads) {
+                console.log(items ,reads);
+                sum += data.items[0].readings[i].value;
+                i+=1;
+            });
+            avr = (sum/i).toFixed(1);
+            $("#avgtemp").html(avr + " &deg;C");
+            console.log(avr)
+        }
+    });
+});
+
 
 
 $(document).ready(function () {                                                     //getting the psi
@@ -42,7 +76,7 @@ $(document).ready(function () {                                                 
             "date": date()
         },
         success: function (data) {
-            console.log("API status: " + data.api_info.status)
+            console.log("PSI API status: " + data.api_info.status)
             var region = {west:data.items[0].readings.psi_twenty_four_hourly.west, 
                         north:data.items[0].readings.psi_twenty_four_hourly.north,
                         east:data.items[0].readings.psi_twenty_four_hourly.east,
@@ -69,19 +103,19 @@ $(document).ready(function () {                                                 
 
 
 function clicknorth() {
-    document.getElementById("gowhere").innerHTML = "PSI in the north: " + north;
+    document.getElementById("gowhere").innerHTML = "PSI in northen Singapore: " + north;
 }
 
 function clicksouth() {
-    document.getElementById("gowhere").innerHTML = "PSI in the south: " + south;
+    document.getElementById("gowhere").innerHTML = "PSI in southern Singapore: " + south;
 }
 
 function clickeast() {
-    document.getElementById("gowhere").innerHTML = "PSI in the east: " + east;
+    document.getElementById("gowhere").innerHTML = "PSI in eastern Singapore: " + east;
 }
 
 function clickwest() {
-    document.getElementById("gowhere").innerHTML = "PSI in the west: " + west;
+    document.getElementById("gowhere").innerHTML = "PSI in western Singapore: " + west;
 }
 function clickcentral() {
     document.getElementById("gowhere").innerHTML = "PSI in central Singapore: " + central;
